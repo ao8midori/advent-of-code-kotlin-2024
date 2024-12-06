c13ec6f7ef2fb3a6bfb83200575017a3b06140ef
aomidori <aomidori_15@posifro.com>
2024-12-07 02:22:08 +0900

    Day02 準備

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,2 +1,18 @@
-class Day02 {
+fun main() {
+    fun part1(input: List<String>):Int{
+
+        return 0
+    }
+
+    fun part2(input: List<String>):Int{
+
+        return 0
+    }
+
+
+    val inputTest = readInput("Day02_test.txt")
+//    val input = readInput("Day02.txt")
+
+    part1(inputTest).println()
+//    part2(input).println()
 }
